8698f9f541e65330a15cdfdb80226322f38503c1
Kaushik0497 <kaushikrewatkar.sfdc@gmail.com>
2024-11-28 14:38:36 +0530

    First commit

---
+++ java/GitSample/Demo1.java
@@ -0,0 +1,9 @@
+package GitSample;
+
+public class Demo1 
+{
+	public static void main(String[] args) {
+		
+		System.out.println("---running Demo1---");
+	}
+}
---
+++ java/GitSample/Demo2.java
@@ -0,0 +1,9 @@
+package GitSample;
+
+public class Demo2 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("---running Demo2---");
+	}
+}
